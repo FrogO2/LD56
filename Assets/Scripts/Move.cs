bdf4241561447ae20d1582c4df39e871503cf74a
Moyuan <2414927637@qq.com>
2024-10-06 21:39:43 +0800

    Move affected by the number of cells
    set the main camera in the test scene to run "Movebymouse.cs".
    add the function that moving speed can be affected by the number of cells. you can increase the extent of influence by decrease the number of countplusa.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,3 +1,4 @@
+using Cell;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -5,23 +6,27 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed=10.0f,aglspeed=1.0f;
+    public float speed=0.5f,aglspeed=1.0f;
     public float smooth = 1.0f;
+    public float countplusa = 10.0f;
     private float tiltAngle = 0f;
     private Vector3 velocity = Vector3.zero;
     
-    private void FixedUpdate()
+    private void Update()
     {
-        
+        float count=MonoCellManager.Instance.MonoCellList.Count;
+        float x = (count + countplusa) / count;
         float moveX = Input.GetAxis("Horizontal");
         tiltAngle += -aglspeed * moveX;
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(0, 0, tiltAngle), smooth);
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(0, 0, tiltAngle), Time.deltaTime*smooth* x);
     }
     private void LateUpdate()
     {
+        float count = MonoCellManager.Instance.MonoCellList.Count;
+        float x = (count + countplusa) / count;
         float moveY = Input.GetAxis("Vertical");
         Vector3 dir= new(math.sin(-tiltAngle*math.PI/180), math.cos(-tiltAngle * math.PI / 180), 0);
-        Vector3 endPos = transform.position + dir * speed * moveY;
-        transform.position = Vector3.SmoothDamp(transform.position, endPos, ref velocity, smooth);
+        Vector3 endPos = transform.position + dir * speed * moveY/x;
+        transform.position = Vector3.SmoothDamp(transform.position, endPos, ref velocity, Time.deltaTime * smooth);
     }
 }
--- Assets/Scripts/Movebymouse.cs
+++ Assets/Scripts/Movebymouse.cs
@@ -1,14 +1,16 @@
+using Cell;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movebymouse : MonoBehaviour
 {
-    public float speed = 10.0f, aglspeed = 1.0f;
+    public float speed = 0.5f;
+    public float countplusa = 10.0f;
     public float smooth = 1.0f;
     private Vector3 velocity = Vector3.zero;
 
-    private void FixedUpdate()
+    private void Update()
     {
         //Vector3 mouse = Input.mousePosition;
         //mouse.z=Camera.main.nearClipPlane;
@@ -27,14 +29,19 @@ public class Movebymouse : MonoBehaviour
         dir.z = 0f;
         dir= dir.normalized;
         float angle = Vector3.Angle(dir,Vector3.up);
-        //if (mouse.x > obj.x) angle = -angle;
-        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), Time.deltaTime);
-        transform.up= dir;
+        float count = MonoCellManager.Instance.MonoCellList.Count;
+        float x = (count + countplusa) / count;
+        if (mouse.x > obj.x) angle = -angle;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), Time.deltaTime * smooth * x);
+        //transform.up= dir;
     }
     private void LateUpdate()
     {
+        float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
-        Vector3 endPos = transform.position + transform.up * speed * moveY;
-        transform.position = Vector3.SmoothDamp(transform.position, endPos, ref velocity, smooth);
+        float count = MonoCellManager.Instance.MonoCellList.Count;
+        float x = (count + countplusa) / count;
+        Vector3 endPos = transform.position + transform.right * speed * moveX /x+ transform.up * speed * moveY / x;
+        transform.position = Vector3.SmoothDamp(transform.position, endPos, ref velocity, Time.deltaTime * smooth);
     }
 }
