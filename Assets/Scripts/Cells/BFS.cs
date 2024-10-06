ec13b928a42b0b73cd0a278167bb47b45dbbab7c
FrogO2 <1302207844@qq.com>
2024-10-06 23:46:11 +0800

    BFS基础函数

--- Assets/Scripts/Cells/BFS.cs
+++ Assets/Scripts/Cells/BFS.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 
 namespace Cell
 {
+
+    public enum Direction
+    {
+        Up,
+        RightUp,
+        RightDown,
+        Down,
+        LeftDown,
+        LeftUp,
+        None
+    }
+    
     public class BFS
     {
         MonoCellManager cellManager;
@@ -12,8 +25,6 @@ namespace Cell
         {
             cellManager = MonoCellManager.Instance;
             cells = cellManager._cells;
-            
-
         }
         
         void Start()
@@ -22,28 +33,100 @@ namespace Cell
             // cellManager.UpCell()
         }
 
-        private List<int> GetConnectedCells(int id, ComponentType type)
+        private int[] GetConnectedCells(int id, ComponentType type, Direction lastDirection)
         {
-            List<int> ids = new List<int>();
 
-            int[] index = {cellManager.UpCell(id), cellManager.RightUpCell(id), cellManager.RightDownCell(id), cellManager.DownCell(id), cellManager.LeftDownCell(id), cellManager.LeftUpCell(id)};
+            // 获取相邻六个方向的棋子的索引
+            int[] index = {
+                cellManager.UpCell(id),
+                cellManager.RightUpCell(id),
+                cellManager.RightDownCell(id),
+                cellManager.DownCell(id),
+                cellManager.LeftDownCell(id),
+                cellManager.LeftUpCell(id)
+            };
 
-            for(int i = 0; i < index.Length; i++)
+            // 遍历每个方向的相邻棋子
+            for (int i = 0; i < index.Length; i++)
             {
-                if (index[i] != -1)
+                if (index[i] != -1) // 如果该方向有相邻棋子
                 {
-                    ids.Add(index[i]);
+                    CellView cellView = cellManager._cells[index[i]]; // 获取相邻棋子的CellView
+
+                    // 根据方向i，判断当前棋子的边是否与相邻棋子的对应边颜色一致
+                    switch (i)
+                    {
+                        case 0: // 上方向
+                            if (!(cellView.m_cell.m_components.down == type && lastDirection != Direction.Down)) // 当前棋子的上边与相邻棋子的下边对比
+                            {
+                                index[i] = -1;
+                            }
+                            break;
+                            
+                        case 1: // 右上方向
+                            if (!(cellView.m_cell.m_components.left_down == type && lastDirection != Direction.LeftDown)) // 当前棋子的右上边与相邻棋子的左下边对比
+                            {
+                                index[i] = -1;
+                            }
+                            break;
+
+                        case 2: // 右下方向
+                            if (!(cellView.m_cell.m_components.left_up == type && lastDirection != Direction.LeftUp)) // 当前棋子的右下边与相邻棋子的左上边对比
+                            {
+                                index[i] = -1;
+                            }
+                            break;
+
+                        case 3: // 下方向
+                            if (!(cellView.m_cell.m_components.up == type && lastDirection != Direction.Up)) // 当前棋子的下边与相邻棋子的上边对比
+                            {
+                                index[i] = -1;
+                            }
+                            break;
+
+                        case 4: // 左下方向
+                            if (!(cellView.m_cell.m_components.right_up == type && lastDirection != Direction.RightUp)) // 当前棋子的左下边与相邻棋子的右上边对比
+                            {
+                                index[i] = -1;
+                            }
+                            break;
+
+                        case 5: // 左上方向
+                            if (!(cellView.m_cell.m_components.right_down == type && lastDirection != Direction.RightDown)) // 当前棋子的左上边与相邻棋子的右下边对比
+                            {
+                                index[i] = -1;
+                            }
+                            break;
+                    }
                 }
             }
-            
-            return ids;
+            return index; // 按顺序返回所有棋子，不符合的为-1
         }
-        
+            
 
-        private bool IsExit(int id)
+        private Direction IsExit(int id)
         {
-            // cellManager._cells[id]
-            return false;
+            int[] index = {
+                cellManager.UpCell(id),
+                cellManager.RightUpCell(id),
+                cellManager.RightDownCell(id),
+                cellManager.DownCell(id),
+                cellManager.LeftDownCell(id),
+                cellManager.LeftUpCell(id)
+            };
+            
+            CellView cellView = cellManager._cells[id];
+            
+            var components = cellView.m_cell.m_components.allcomponents;
+            
+            for (int i = 0; i < index.Length; i++)
+            {
+                if (index[i] != -1 && components[id] == ComponentType.Exhaust)
+                {
+                    return (Direction)i;
+                }
+            }
+            return Direction.None;
         }
     }
 }
