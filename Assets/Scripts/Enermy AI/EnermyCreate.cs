f4669d4e4e4d1f612a789724e712dbffa5b36047
lee514532 <121090258@link.cuhk.edu.cn>
2024-10-07 21:44:29 +0800

    吸取
    吸取

--- Assets/Scripts/Enermy AI/EnermyCreate.cs
+++ Assets/Scripts/Enermy AI/EnermyCreate.cs
@@ -9,6 +9,10 @@ public class EnermyCreate : MonoBehaviour
     public CameraController CameraController;
     public GameObject OB;
     public GameObject Robot;
+    public GameObject OB1;
+    public GameObject OB2;
+    public GameObject OB3;
+    public Vector4 Posibility;
     public int EnermyNumber = 100;
     public int EA=0;
     public int E=0;
@@ -29,7 +33,7 @@ public class EnermyCreate : MonoBehaviour
             GameObject copy = Instantiate(OB);
             float a1=Random.Range(-ra,ra);
             float a2=Random.Range(-ra,ra);
-            float a3=Random.Range(-ra,ra);
+            float a3=Random.Range(-100,100);
             Vector2 k=CameraController.Po();
             while(i1<EnermyNumber){
                 a1=Random.Range(-ra,ra);
@@ -37,14 +41,31 @@ public class EnermyCreate : MonoBehaviour
                 if((a1-k[0])*(a1-k[0])+(a2-k[1])*(a2-k[1])<-600||(a1-k[0])*(a1-k[0])+(a2-k[1])*(a2-k[1])>600){
                     break;}
                 }
-            if((a3<20&&a3>-20)&&E<EA){
+            if((a3<Posibility[0]&&a3>-Posibility[0])&&E<EA){
             GameObject copy1 = Instantiate(Robot);
             copy1.transform.position = new Vector3(a1, a2, 0);
             i1+=1;
             E+=1;
             }
+            else if(a3<Posibility[1]+Posibility[0]&&a3>-Posibility[0]-Posibility[1]){
+            GameObject copy1 = Instantiate(OB1);
+            copy1.transform.position = new Vector3(a1, a2, 0);
+            i1+=1;
+            }
+            else if(a3<Posibility[1]+Posibility[0]+Posibility[2]&&a3>-Posibility[0]-Posibility[1]-Posibility[2]){
+            GameObject copy1 = Instantiate(OB2);
+            copy1.transform.position = new Vector3(a1, a2, 0);
+            i1+=1;
+            }
+            else if(a3<Posibility[1]+Posibility[0]+Posibility[2]+Posibility[3]&&a3>-Posibility[0]-Posibility[1]-Posibility[2]-Posibility[3]){
+            GameObject copy1 = Instantiate(OB3);
+            copy1.transform.position = new Vector3(a1, a2, 0);
+            i1+=1;
+            }
+            else{
             copy.transform.position = new Vector3(a1, a2, 0);
             i1+=1;
+            }
         }
         
     }
--- Assets/Scripts/Enermy AI/EnermyDead.cs
+++ Assets/Scripts/Enermy AI/EnermyDead.cs
@@ -4,9 +4,48 @@ using UnityEngine;
 
 public class EnermyDead : MonoBehaviour
 {
-    public EnermyCreate EnermyCreate;
-    private void OnTriggerEnter2D(Collider2D other){
-Destroy(gameObject);
-EnermyCreate.de();
+    public float k1;
+    private Rigidbody2D rb; 
+    private Vector2 offset;
+    public GameObject square;
+    private Vector2 movement;
+    private EnermyCreate enermyCreate;
+    public float res;
+    public Cell.MonoCell Mono;
+    public float scale;
+    public List<int> t=new List<int>(100);
+    private void OnTriggerEnter2D(Collider2D collision){
+        switch (collision.tag){
+        case "Cell":
+        Destroy(gameObject);
+        enermyCreate.de();
+        break;
+        case "Ai":
+        Destroy(gameObject);
+        break;
+        default:break;
+        }
     }
+    void Awake(){
+        GameObject a = GameObject.Find("EventSystem (1)");
+        enermyCreate=a.GetComponent<EnermyCreate>();
+    }
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+void Update()
+    {
+}
+    public void run1(float force){
+    Vector2  sq= transform.position;
+    offset = sq - new Vector2(-1,-1);
+    offset =-offset;
+    movement= offset.normalized;
+    rb.velocity = movement*force*10; 
+    }
+    public void stop1(){
+    movement= new Vector2(0,0);
+    rb.velocity = movement*10;
+    }
+
 }
