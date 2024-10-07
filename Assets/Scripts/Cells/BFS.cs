66809b1e3c2367d6c5f286594bcee4d22997e2ad
FrogO2 <1302207844@qq.com>
2024-10-08 02:26:36 +0800

    BFS

--- Assets/Scripts/Cells/BFS.cs
+++ Assets/Scripts/Cells/BFS.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
@@ -17,39 +18,35 @@ namespace Cell
         LeftUp,
         None=-1
     }
+
+    public struct Organelle
+    {
+        public ComponentType type;
+        public Direction direction;
+        public int id;
+    }
     
     
     
     public class BFS
     {
         MonoCellManager cellManager;
+
+        public Organelle NewOrganelle(ComponentType type, Direction direction, int id)
+        {
+            var o = new Organelle();
+            o.type = type;
+            o.direction = direction;
+            o.id = id;
+            return o;
+        }
         
         public BFS()
         {
             cellManager = MonoCellManager.Instance;
         }
         
-        public Direction GetRelativeDirection(int id1, int id2)
-        {
-            // 获取 id1 相邻六个方向的 ID
-            int leftUpId = cellManager.LeftUpCell(id1);
-            int rightUpId = cellManager.RightUpCell(id1);
-            int leftDownId = cellManager.LeftDownCell(id1);
-            int rightDownId = cellManager.RightDownCell(id1);
-            int upId = cellManager.UpCell(id1);
-            int downId = cellManager.DownCell(id1);
 
-            // 判断 id2 是否与 id1 相邻
-            if (id2 == leftUpId) return Direction.LeftUp;
-            if (id2 == rightUpId) return Direction.RightUp;
-            if (id2 == leftDownId) return Direction.LeftDown;
-            if (id2 == rightDownId) return Direction.RightDown;
-            if (id2 == upId) return Direction.Up;
-            if (id2 == downId) return Direction.Down;
-
-            // 如果 id2 不在 id1 的相邻方向，返回 None
-            return Direction.None;
-        }
         
         /// <summary>
         /// 使用BFS搜索相连的管道
@@ -58,24 +55,25 @@ namespace Cell
         /// <param name="type">组件类型</param>
         /// <param name="direction">初始方向</param>
         /// <returns>相连管道的ID列表</returns>
-        public List<int> GetConnectedPipes(int startId, ComponentType type, Direction direction)
+        public Tuple<List<int>, List<Organelle>> GetConnectedPipes(int startId, ComponentType type, Direction direction)
         {
             Queue<(int, Direction)> queue = new Queue<(int, Direction)>(); // BFS队列，包含当前cell的ID和它的方向
             HashSet<int> visited = new HashSet<int>(); // 已访问的cell
             List<int> connectedCells = new List<int>(); // 存储连通的cell
-
-            // 初始起点入队，方向为Direction.None
+            List<Organelle> connectedOrganelles = GetConnectedComponents(startId, type, direction); // 存储连通的organelles
+            
             queue.Enqueue((startId, direction));
             visited.Add(startId);
             connectedCells.Add(startId);
 
+            
             // 开始BFS
             while (queue.Count > 0)
             {
                 var (currentId, lastDirection) = queue.Dequeue(); // 取出当前节点
 
                 // 获取与当前节点相连的节点
-                int[] connectedIds = GetConnectedCells(currentId, type, lastDirection);
+                var connectedIds = GetConnectedCells(currentId, type, lastDirection);
 
                 // 遍历所有相连的节点
                 for (int i = 0; i < connectedIds.Length; i++)
@@ -88,88 +86,101 @@ namespace Cell
                         queue.Enqueue((neighborId, (Direction)i)); // 将相邻节点入队，并记录方向
                         visited.Add(neighborId); // 标记为已访问
                         connectedCells.Add(neighborId); // 添加到结果列表
+                        foreach (var organelle in GetConnectedComponents(neighborId, type, (Direction)i))
+                        {
+                            connectedOrganelles.Add(organelle);
+                        }
                     }
                 }
             }
 
-            return connectedCells;
+            return new Tuple<List<int>, List<Organelle>>(connectedCells, connectedOrganelles);
         }
 
-        private List<int> GetConnectedProduce(int id, Direction lastDirection)
+        private List<Organelle> GetConnectedProduce(Organelle organelle)
         {
-            List<int> connected = new List<int>();
-            ComponentType type = ComponentType.Produce;
-            var components = cellManager._cells[id].m_cell.m_components.allcomponents;
+            List<Organelle> connected = new List<Organelle>();
+            var components = cellManager._cells[organelle.id].m_cell.m_components.allcomponents;
 
-            if (components[((int)lastDirection + 2) % 6]==type)
+            if (components[((int)organelle.direction + 1) % 6]==ComponentType.Produce)
             {
-                connected.Add(((int)lastDirection + 2) % 6);
+                connected.Add(NewOrganelle(ComponentType.Produce, (Direction)(((int)organelle.direction + 1) % 6), organelle.id));
             }
             
-            if (components[((int)lastDirection + 4) % 6]==type)
+            if (components[((int)organelle.direction - 1) % 6]==ComponentType.Produce)
             {
-                connected.Add(((int)lastDirection + 4) % 6);
+                connected.Add(NewOrganelle(ComponentType.Produce, (Direction)(((int)organelle.direction - 1) % 6), organelle.id));
             }
 
             return connected;
 
         }
 
-        public List<List<int>> GetAll()
+        public Tuple<List<Organelle>, List<List<int>>, List<List<Organelle>>, List<List<int>>, List<List<Organelle>>> GetAll()
         {
             // List<int> availableIds = new List<int>();
             // 找到所有可用的入口
-            List<int[]> availableEntrances = new List<int[]>();
+            List<Organelle> availableEntrances = new List<Organelle>();
             for (int i = 0; i < cellManager.CheckMap.Length; i++)
             {
                 if (cellManager.CheckMap[i])
                 {
-                    var entry = IsEntrance(i);
-                    if (entry != Direction.None)
+                    var entrances = IsEntrance(i);
+                    foreach (var entrance in entrances)
                     {
-                        int[] j = { i, (int)entry };
-                        availableEntrances.Add(j);
+                        availableEntrances.Add(entrance);
                     }
                 }
             }
-
-
-            var devours = new List<int[]>();
+            
+            var devours = new List<Organelle>();
             var producePipes = new List<List<int>>();
             var exhaustPipes = new List<List<int>>();
+            var producePipesOrganelles = new List<List<Organelle>>();
+            var exhaustPipesOrganelles = new List<List<Organelle>>();
             
             // 找到每个入口相连的能量细胞器
             foreach (var entrance in availableEntrances)
             {
-                var produces = GetConnectedProduce(entrance[0], (Direction)entrance[1]);
-                var tempDevours = new List<int[]>();
-                var tempProducePipes = new List<List<int>>();
+                var produces = GetConnectedProduce(entrance);
 
                 foreach (var produce in produces)
                 {
-                    var producePipe = GetConnectedPipes(entrance[0], ComponentType.Produce, (Direction)produce);
-                    Direction lastDirection = (Direction)produce;
+                    (var producePipe, var produceOrganelles) = GetConnectedPipes(produce.id, ComponentType.Produce, produce.direction);
                     
-                    var exhausts = new List<int[]>();
+                    var exhausts = new HashSet<Organelle>();
                     // 找到能量相连的排泄
-                    for (int i = 0; i < producePipe.Count - 1; i++)
+                    
+                    foreach (var produceOrganelle in produceOrganelles)
                     {
-                        var ex = GetConnectedDevour(producePipe[i], lastDirection);
-
-                        for (int j = 0; j < ex.Length; j++)
+                        foreach (var exhaust in GetConnectedExhaust(produceOrganelle))
                         {
-                            if (ex[i] != -1)
-                            {
-                                exhausts.Add(new int[] {ex[j], j});
-                            }
+                            exhausts.Add(exhaust);
                         }
-                        
-                        lastDirection = GetRelativeDirection(producePipe[i], producePipe[i + 1]);
                     }
 
                     foreach (var exhaust in exhausts)
                     {
-                        GetConnectedPipes(exhaust[0], ComponentType.Exhaust, (Direction)exhaust[1]);
+                        (var exhaustPipe, var exhaustOrganelles) = GetConnectedPipes(exhaust.id, ComponentType.Exhaust, exhaust.direction);
+
+                        var flag = false;
+                        foreach (var ex in exhaustOrganelles)
+                        {
+                            if (IsExit(ex))
+                            {
+                                flag = true;
+                                break; 
+                            }
+                        }
+
+                        if (flag)
+                        {
+                            devours.Add(entrance);
+                            producePipes.Add(producePipe);
+                            producePipesOrganelles.Add(produceOrganelles);
+                            exhaustPipes.Add(exhaustPipe);
+                            exhaustPipesOrganelles.Add(exhaustOrganelles);
+                        }
                     }
                     tempDevours.Add(entrance);
                     //tempProducePipes.Add();
@@ -178,28 +189,35 @@ namespace Cell
                 
             }
 
-            return new List<List<int>>();
+            return new Tuple<List<Organelle>, List<List<int>>, List<List<Organelle>>, List<List<int>>, List<List<Organelle>>>(devours, producePipes, producePipesOrganelles, exhaustPipes, exhaustPipesOrganelles);
         }
 
-        private int[] GetConnectedDevour(int id, Direction direction)
+        // 获取对面或身边的排泄模块[ID, 方向]
+        private List<Organelle> GetConnectedExhaust(Organelle organelle)
         {
-            int[] connected = {-1, -1, -1, -1, -1, -1};
+            List<Organelle> connected = new List<Organelle>();
             int[] surroundingCells = {
-                cellManager.UpCell(id),
-                cellManager.RightUpCell(id),
-                cellManager.RightDownCell(id),
-                cellManager.DownCell(id),
-                cellManager.LeftDownCell(id),
-                cellManager.LeftUpCell(id)
+                cellManager.UpCell(organelle.id),
+                cellManager.RightUpCell(organelle.id),
+                cellManager.RightDownCell(organelle.id),
+                cellManager.DownCell(organelle.id),
+                cellManager.LeftDownCell(organelle.id),
+                cellManager.LeftUpCell(organelle.id)
             };
-            
-            var connectedComponents = GetConnectedComponents(id, ComponentType.Produce, direction);
-            for (int i = 0; i < connectedComponents.Count; i++)
+
+            if (ComponentType.Exhaust == cellManager._cells[surroundingCells[(int)organelle.direction]].m_cell.m_components.allcomponents[((int)organelle.direction + 3) % 6])
             {
-                if (ComponentType.Produce == cellManager._cells[surroundingCells[i]].m_cell.m_components.allcomponents[(i + 3) % 6])
-                {
-                    connected[i] = surroundingCells[i];
-                }
+                connected.Add(NewOrganelle(ComponentType.Exhaust, (Direction)(((int)organelle.direction + 3) % 6), surroundingCells[(int)organelle.direction]));
+            }
+            
+            if (cellManager._cells[organelle.id].m_cell.m_components.allcomponents[((int)organelle.direction + 1) % 6]==ComponentType.Exhaust)
+            {
+                connected.Add(NewOrganelle(ComponentType.Exhaust, (Direction)(((int)organelle.direction + 1) % 6), organelle.id));
+            }
+        
+            if (cellManager._cells[organelle.id].m_cell.m_components.allcomponents[((int)organelle.direction - 1) % 6]==ComponentType.Exhaust)
+            {
+                connected.Add(NewOrganelle(ComponentType.Exhaust, (Direction)(((int)organelle.direction - 1) % 6), organelle.id));
             }
 
             return connected;
@@ -213,7 +231,7 @@ namespace Cell
         /// <param name="id">要检查的细胞的ID。</param>
         /// <param name="type">要检查的细胞器类型。</param>
         /// <param name="lastDirection">上一个相邻细胞的方向。</param>
-        /// <returns>按顺序返回所有符合细胞器类型连接的相邻细胞ID，不符合的为-1。</returns>
+        /// <returns>按顺序返回所有符合细胞器类型连接的相邻细胞ID，不符合的为-1，同时返回所有相连的细胞器。</returns>
         private int[] GetConnectedCells(int id, ComponentType type, Direction lastDirection)
         {
             // 获取相邻六个方向的棋子的索引
@@ -227,12 +245,15 @@ namespace Cell
             };
             
             int[] index = {-1, -1, -1, -1, -1, -1};
+            
+            List<Organelle> organelles = GetConnectedComponents(id, type, lastDirection);
 
-            foreach (int i in GetConnectedComponents(id, type, lastDirection))
+            foreach (Organelle i in organelles)
             {
-                if (type == cellManager._cells[surroundingCells[i]].m_cell.m_components.allcomponents[(i + 3) % 6])
+                if (type == cellManager._cells[surroundingCells[(int)i.direction]].m_cell.m_components.allcomponents[((int)i.direction + 3) % 6])
                 {
-                    index[i] = surroundingCells[i];
+                    index[(int)i.direction] = surroundingCells[(int)i.direction];
+
                 }
             }
 
@@ -245,12 +266,10 @@ namespace Cell
         /// <param name="id">要检查的细胞的ID。</param>
         /// <param name="type">要检查的细胞器类型。</param>
         /// <param name="lastDirection">上一个相邻细胞的方向。</param>
-        /// <returns>返回一个列表，包含所有从相邻细胞进入当前细胞的连续细胞器下标。</returns>
-        private List<int> GetConnectedComponents(int id, ComponentType type, Direction lastDirection)
+        /// <returns>返回一个列表，包含所有从相邻细胞进入当前细胞的连续细胞器。</returns>
+        private List<Organelle> GetConnectedComponents(int id, ComponentType type, Direction lastDirection)
         {
-            List<int> index = new List<int>();
-
-            var organelles = cellManager._cells[id].m_cell.m_components.allcomponents;
+            List<Organelle> organelles = new List<Organelle>();
 
             int from = ((int)lastDirection + 3) % 6;
 
@@ -258,10 +277,10 @@ namespace Cell
             // 遍历顺时针的相邻细胞器
             for (int i = from; i < from + 6; i++)
             {
-                if (type == organelles[i % 6])
+                if (type == cellManager._cells[id].m_cell.m_components.allcomponents[i % 6])
                 {
                     thres--;
-                    index.Add(i % 6);
+                    organelles.Add(NewOrganelle(type, (Direction)(i % 6), id));
                 }
                 else
                 {
@@ -271,16 +290,16 @@ namespace Cell
             // 逆时针
             for (int i = from; i > from - thres; i--)
             {
-                if (type == organelles[i % 6])
+                if (type == cellManager._cells[id].m_cell.m_components.allcomponents[i % 6])
                 {
-                    index.Add(i % 6);
+                    organelles.Add(NewOrganelle(type, (Direction)(i % 6), id));
                 }
                 else
                 {
                     break;
                 }
             }
-            return index;
+            return organelles;
         }
         
         
@@ -314,36 +333,28 @@ namespace Cell
             return Direction.None;
         }
         
-        private Direction IsExit(int id, Direction direction)
+        private bool IsExit(Organelle organelle)
         {
             int[] index = {
-                cellManager.UpCell(id),
-                cellManager.RightUpCell(id),
-                cellManager.RightDownCell(id),
-                cellManager.DownCell(id),
-                cellManager.LeftDownCell(id),
-                cellManager.LeftUpCell(id)
+                cellManager.UpCell(organelle.id),
+                cellManager.RightUpCell(organelle.id),
+                cellManager.RightDownCell(organelle.id),
+                cellManager.DownCell(organelle.id),
+                cellManager.LeftDownCell(organelle.id),
+                cellManager.LeftUpCell(organelle.id)
             };
             
-            var components = GetConnectedComponents(id, ComponentType.Exhaust, direction);
-            
-            for (int i = 0; i < index.Length; i++)
+            if (index[(int)organelle.direction] == -1)
             {
-                if (index[i] == -1 && components.Contains(i))
-                {
-                    return (Direction)i;
-                }
+                return true;
             }
-            return Direction.None;
+            return false;
         }
         
-        /// <summary>
-        /// Checks if the specified cell has an Devour Component with no obstacles and returns the direction of the entry.
-        /// </summary>
-        /// <param name="id">The ID of the cell to check.</param>
-        /// <returns>The direction of the entry if found, otherwise Direction.None.</returns>
-        private Direction IsEntrance(int id)
+        private List<Organelle> IsEntrance(int id)
         {
+            List<Organelle> organelles = new List<Organelle>();
+            
             int[] index = {
                 cellManager.UpCell(id),
                 cellManager.RightUpCell(id),
@@ -361,10 +372,11 @@ namespace Cell
             {
                 if (index[i] == -1 && components[i] == ComponentType.Devour)
                 {
-                    return (Direction)i;
+                    organelles.Add(NewOrganelle(ComponentType.Devour, (Direction)i, id));
                 }
             }
-            return Direction.None;
+
+            return organelles;
         }
     }
 }
